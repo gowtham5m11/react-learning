1b732d965be6cf56c37e1a55a123c0099cc8d2be
gowtham5m11 <gowthamjadapalli@gmail.com>
2025-03-07 09:53:12 +0530

    Update ListGroup.jsx

--- vite-project/src/components/ListGroup.jsx
+++ vite-project/src/components/ListGroup.jsx
@@ -2,6 +2,7 @@ import { Fragment } from "react";
 function ListGroup() {
   return (
     <Fragment>
+      <h1>List group</h1>
       <ul className="list-group">
         <li className="list-group-item">An item</li>
         <li className="list-group-item">A second item</li>
