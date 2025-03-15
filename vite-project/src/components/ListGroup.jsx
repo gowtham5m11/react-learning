f034789e15b1b966561c8620e68d5bd39982d463
gowtham5m11 <gowthamjadapalli@gmail.com>
2025-03-15 16:56:42 +0530

    video lo cheppinattu chesthe raledhu but co pilot ni adigithe ochhindi problem       {items.map ( (item) ‹li>{item}</li>)} solution      <li key={item.id}>{item.name}</li>)}

--- vite-project/src/components/ListGroup.jsx
+++ vite-project/src/components/ListGroup.jsx
@@ -1,14 +1,18 @@
 import { Fragment } from "react";
 function ListGroup() {
+  const items = [
+    { id: 1, name: "1" },
+    { id: 2, name: "2" },
+    { id: 3, name: "3" },
+    { id: 4, name: "4" },
+    { id: 5, name: "5" }, 
+  ];
   return (
     <Fragment>
       <h1>List group</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+      {items.map((item) => (
+      <li key={item.id}>{item.name}</li>))}
       </ul>
     </Fragment>
   );
