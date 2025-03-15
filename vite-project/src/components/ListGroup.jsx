41d4c00a854ee2f28967de00796927fbd6be110f
gowtham5m11 <gowthamjadapalli@gmail.com>
2025-03-15 19:01:01 +0530

    removed duplication and added conditions inside the ListGroup component

--- vite-project/src/components/ListGroup.jsx
+++ vite-project/src/components/ListGroup.jsx
@@ -8,11 +8,10 @@ function ListGroup() {
     { id: 5, name: "5" }, 
   ];
   items=[];
-  if (items.length ===0)
-    return <><h1>List group</h1><p>There are no items in the list</p></>; 
   return (
     <Fragment>
       <h1>List group</h1>
+      {items.length===0 ? <p>No items to display</p> : <p>Items to display</p>}
       <ul className="list-group">
       {items.map((item) => (
       <li key={item.id}>{item.name}</li>))}
